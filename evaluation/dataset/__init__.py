be90a29d8f1d883611c552c222ddbd6cd813bbe1
Xiangkui Cao <caoxiangkui19@mails.ucas.ac.cn>
2024-06-14 10:07:04 +0800

    Update __init__.py

--- evaluation/dataset/__init__.py
+++ evaluation/dataset/__init__.py
@@ -1,4 +1,4 @@
-from .VLBOLDDataset import VLBOLDDataset, VLBOLDXLDataset
 from .close_ended import close_ended_dataset
+from .open_ended import open_ended_dataset
 
 __all__ = ['open_ended_dataset', 'close_ended_dataset']
