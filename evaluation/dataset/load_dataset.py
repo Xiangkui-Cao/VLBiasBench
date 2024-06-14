6dc1c7e2b92fd299c2c22f57d6b3c11f3a0644bd
Xiangkui Cao <caoxiangkui19@mails.ucas.ac.cn>
2024-06-14 10:06:11 +0800

    Update load_dataset.py

--- evaluation/dataset/load_dataset.py
+++ evaluation/dataset/load_dataset.py
@@ -3,11 +3,7 @@ from dataset import *
 
 dataset_dict = {
     ### Bias Dataset ###
-    "vlbold-g": VLBOLDDataset(dataset_name="vlbold-g", type="gender"),
-    "vlbold-pi": VLBOLDDataset(dataset_name="vlbold-pi", type="political_ideology"),
-    "vlbold-p": VLBOLDDataset(dataset_name="vlbold-p", type="profession"),
-    "vlbold-r": VLBOLDDataset(dataset_name="vlbold-r", type="race"),
-    "vlbold-ri": VLBOLDDataset(dataset_name="vlbold-ri", type="religious_ideology"),
+    "open_ended_dataset": open_ended_dataset(dataset_name="open_ended_dataset"),
     "close_ended_dataset": close_ended_dataset(dataset_name="close_ended_dataset")
 }
 
